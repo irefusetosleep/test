d3b5e7c8ddf7dad5f25daff27de6fba8404b4570
irefusetosleep <68473299+irefusetosleep@users.noreply.github.com>
2021-11-12 18:06:57 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
-for x in range(1, 11):
-    print(x)
+# (str) Title of your application 
+title = "TestApp"
+
+# (str) Package name
+package.name = "main.py"
+
+# (str) Package domain (needed for android/ios packaging)
+package.domain = "com.TestThingy"
+
+# (list) Application requirements
+# comma separated e.g. requirements = sqlite3,kivy
+requirements = python3,kivy, kivymd
+
+# (list) Permissions
+android.permissions = INTERNET
